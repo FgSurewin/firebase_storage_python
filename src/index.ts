43ffdedb6a923a8007ee1725d09b64ba716fafaa
FgSurewin <mrliu3244794@gmail.com>
2023-03-30 15:40:09 -0400

    update  -v1

--- src/index.ts
+++ src/index.ts
@@ -6,66 +6,102 @@ import { db } from "./firebase";
 import { getDoc, doc } from "firebase/firestore";
 import { Query_Firestore } from "./query_database";
 import cliProgress from "cli-progress";
+import { readFile, writeFile, promises } from "fs";
 
 async function query_from_storage() {
-	/* -------------------------------------------------------------------------- */
-	/*                                 Instruction                                */
-	/* -------------------------------------------------------------------------- */
-	/**
-	 * How to use Query class:
-	 * 1. Create an instance of Query class
-	 * 2. Call download() method to download the files.
-	 *
-	 * Useful methods:
-	 * 1. Call getAllUsers() method to get all users.
-	 * 2. Call getUserVideos(user: StorageReference) method to get all videos of a user.
-	 * 3. Call getUserCSV(user: StorageReference) method to get all csv files of a user.
-	 * 4. Call writeToFile(data, outputFileName) method to write data to a file.
-	 * 5. Call getFileNameAndUrl() method to get all files recursively.
-	 *
-	 *
-	 * Note:
-	 * The all_files.json file will be generated in the `output` directory
-	 * when you initialize the class with init() method.
-	 * if the all_files.json file already exists, it will be overwritten.
-	 * Also, if the all_files.json file is not generated, you can call the writeToFile(data, outputFileName) method to generate it.
-	 * By the way, you can use writeToFile(data, outputFileName) method to generate whatever files you want.
-	 */
+  /* -------------------------------------------------------------------------- */
+  /*                                 Instruction                                */
+  /* -------------------------------------------------------------------------- */
+  /**
+   * How to use Query class:
+   * 1. Create an instance of Query class
+   * 2. Call download() method to download the files.
+   *
+   * Useful methods:
+   * 1. Call getAllUsers() method to get all users.
+   * 2. Call getUserVideos(user: StorageReference) method to get all videos of a user.
+   * 3. Call getUserCSV(user: StorageReference) method to get all csv files of a user.
+   * 4. Call writeToFile(data, outputFileName) method to write data to a file.
+   * 5. Call getFileNameAndUrl() method to get all files recursively.
+   *
+   *
+   * Note:
+   * The all_files.json file will be generated in the `output` directory
+   * when you initialize the class with init() method.
+   * if the all_files.json file already exists, it will be overwritten.
+   * Also, if the all_files.json file is not generated, you can call the writeToFile(data, outputFileName) method to generate it.
+   * By the way, you can use writeToFile(data, outputFileName) method to generate whatever files you want.
+   */
 
-	/* ------------------------------ Basic Usage: ------------------------------ */
-	const queryInstance = new Query(
-		storageDirectory,
-		rootDirectory,
-		outputFolderName
-	);
-	await queryInstance.download();
-	console.log("-> End!");
+  /* ------------------------------ Basic Usage: ------------------------------ */
+  const queryInstance = new Query(
+    storageDirectory,
+    rootDirectory,
+    outputFolderName
+  );
+  await queryInstance.download();
+  console.log("-> End!");
 }
 
 async function query_from_firestore() {
-	const usersRef = doc(db, "users", "authenticated_users");
-	const usersSnap = await getDoc(usersRef);
-	const usersRes = usersSnap.data() as { user_list: string[] };
-	const choices = [...usersRes.user_list, "all"];
-	const queryInstance = new Query_Firestore(
-		storageDirectory,
-		rootDirectory,
-		outputFolderName,
-		usersRes.user_list
-	);
-	console.log(
-		"-> Please select a user to query his/her files or select 'all' to query all files):"
-	);
-	const result = await prompt([
-		{
-			type: "list",
-			name: "user",
-			choices: choices,
-			loop: false,
-		},
-	]);
-	queryInstance.query(result.user);
+  const usersRef = doc(db, "users", "authenticated_users");
+  const usersSnap = await getDoc(usersRef);
+  const usersRes = usersSnap.data() as { user_list: string[] };
+  const choices = [...usersRes.user_list, "all"];
+  const queryInstance = new Query_Firestore(
+    storageDirectory,
+    rootDirectory,
+    outputFolderName,
+    usersRes.user_list
+  );
+  console.log(
+    "-> Please select a user to query his/her files or select 'all' to query all files):"
+  );
+  const result = await prompt([
+    {
+      type: "list",
+      name: "user",
+      choices: choices,
+      loop: false,
+    },
+  ]);
+  queryInstance.query(result.user);
+}
+
+async function query_from_selected_users() {
+	const users = await promises.readFile("./users.json", "utf-8");
+  const parse_users = JSON.parse(users);
+  const queryInstance = new Query_Firestore(
+    storageDirectory,
+    rootDirectory,
+    outputFolderName,
+    parse_users
+  )
+  // for (const user of parse_users) {
+  //   console.log("-> Querying user: " + user);
+  //   await queryInstance.query(user);
+  //   console.log("-> Done!")
+  // }
+  await queryInstance.query("jiawei-walk-test");
 }
 
 // query_from_storage();
 query_from_firestore();
+// query_from_selected_users();
+
+
+/* ---------------------- Get all users from firestore ---------------------- */
+// async function get_all_users() {
+//   const usersRef = doc(db, "users", "authenticated_users");
+//   const usersSnap = await getDoc(usersRef);
+//   const usersRes = usersSnap.data() as { user_list: string[] };
+//   //  save usersRes.user_list to a json file.
+//   writeFile("./users.json", JSON.stringify(usersRes.user_list), (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("-> Users saved!");
+//   });
+// }
+
+// get_all_users();
--- src/query_database.ts
+++ src/query_database.ts
@@ -8,136 +8,177 @@ import { FirebaseStorage, getDownloadURL, ref } from "firebase/storage";
 import path from "path";
 
 interface UserData {
-	createdAt: string;
-	videoInfo: {
-		videoDuration: number;
-		videoURL: string;
-		endTime: string;
-		startTime: string;
-	};
-	uniqueIdentifier: string;
-	category: string;
-	sensorDataURL: string;
+  account: {
+    email: string;
+    name: string;
+    orgniization: string;
+    "phone name": string;
+  };
+  createdAt: string;
+  videoInfo: {
+    videoDuration: number;
+    videoURL: string[];
+    endTime: string;
+    startTime: string;
+    videoSegementTimestamp: string[];
+  };
+  uniqueIdentifier: string;
+  category: string;
+  sensorDataURL: string[];
+  slowMode: boolean;
+}
+
+interface IRecord {
+  category: string;
+  videoURL: string;
+  sensorDataURL: string;
 }
 
 export class Query_Firestore extends Query_Base {
-	private db: Firestore;
-	private storage: FirebaseStorage;
-	public storageDirectory: string;
-	public rootDirectory: string;
-	public outputFolderName: string;
-	public users: string[];
+  private db: Firestore;
+  private storage: FirebaseStorage;
+  public storageDirectory: string;
+  public rootDirectory: string;
+  public outputFolderName: string;
+  public users: string[];
 
-	constructor(
-		storageDirectory: string,
-		rootDirectory: string,
-		outputFolderName: string,
-		users: string[]
-	) {
-		super();
-		this.db = db;
-		this.storage = storage;
-		this.storageDirectory = storageDirectory;
-		this.rootDirectory = rootDirectory;
-		this.outputFolderName = outputFolderName;
-		this.users = users;
-	}
+  constructor(
+    storageDirectory: string,
+    rootDirectory: string,
+    outputFolderName: string,
+    users: string[]
+  ) {
+    super();
+    this.db = db;
+    this.storage = storage;
+    this.storageDirectory = storageDirectory;
+    this.rootDirectory = rootDirectory;
+    this.outputFolderName = outputFolderName;
+    this.users = users;
+  }
 
-	public async query(user: string) {
-		// TODO Check if the output folder exists
-		await this.checkFolderExists(
-			path.join(this.rootDirectory, this.outputFolderName)
-		);
-		if (user === "all") {
-			// TODO Query all files
-			console.log("-> Downloading all files");
-		} else {
-			await this.queryOneUser(user);
-		}
-	}
+  public async query(user: string) {
+    // TODO Check if the output folder exists
+    await this.checkFolderExists(
+      path.join(this.rootDirectory, this.outputFolderName)
+    );
+    if (user === "all") {
+      // TODO Query all files
+      console.log("-> Downloading all files");
+    } else {
+      await this.queryOneUser(user);
+    }
+  }
 
-	public async queryOneUser(user: string) {
-		const userRef = collection(db, "users", "user_data", user);
-		const usersnap = await getDocs(userRef);
-		const files = usersnap.docs.map((doc) => doc.data() as UserData);
-		// TODO Check if the files are already downloaded
-		const pattern = `${this.rootDirectory}/${this.outputFolderName}/${user}/*.MOV`;
-		const oldFiles = await this.Glob(pattern);
-		const oldFilesTimestamps = oldFiles.map((file) => {
-			const filename = file.split("/").pop() as string;
-			const timestampArr = filename.split("_");
-			const timestamp = timestampArr[0] + "_" + timestampArr[1];
-			return timestamp;
-		});
-		const newFiles = files.filter((file) => {
-			const timestamp = this.changeFileame(file.createdAt);
-			return !oldFilesTimestamps.includes(timestamp);
-		});
-		console.log(
-			`-> File Report |Total: ${files.length} | New: ${newFiles.length} | Exist: ${oldFiles.length}`
-		);
-		// TODO Download new files
-		await this.downloadAllFiles(newFiles, user);
-	}
+  public async queryOneUser(user: string) {
+    const userRef = collection(db, "users", "user_data", user);
+    const usersnap = await getDocs(userRef);
+    const files = usersnap.docs.map((doc) => doc.data() as UserData);
+    // TODO Check if the files are already downloaded
+    const VideoPattern = `${this.rootDirectory}/${this.outputFolderName}/${user}/*.MOV`;
+    const oldVideoFiles = await this.Glob(VideoPattern);
+    // Get filenames
+    const oldVideoFilesNames = oldVideoFiles.map((file) => {
+      const filename = file.split("/").pop() as string;
+      return filename;
+    });
+    const newFiles: IRecord[] = [];
+    files.forEach((file) => {
+      if (file.createdAt !== undefined) {
+        const videoURLs = file.videoInfo.videoURL;
+        const sensorDataURLs = file.sensorDataURL;
+        const category = file.category;
+        const length = file.videoInfo.videoURL.length;
+        for (let i = 0; i < length; i++) {
+          const videoFileName = videoURLs[i].split("/").pop() as string;
+          if (!oldVideoFilesNames.includes(this.changeFileame(videoFileName))) {
+            newFiles.push({
+              category: category,
+              videoURL: videoURLs[i],
+              sensorDataURL: sensorDataURLs[i],
+            });
+          }
+        }
+      }
+    });
 
-	public async getURL(gs: string): Promise<string> {
-		const gsRef = ref(this.storage, gs);
-		const url = await getDownloadURL(gsRef);
-		return url;
-	}
+    // const oldFilesTimestamps = oldFiles.map((file) => {
+    //   const filename = file.split("/").pop() as string;
+    //   const timestampArr = filename.split("_");
+    //   const timestamp = timestampArr[0] + "_" + timestampArr[1];
+    //   return timestamp;
+    // });
+    // const newFiles = files.filter((file) => {
+    //   //   console.log(file.createdAt);
+    //   if (file.createdAt === undefined) return false;
+    //   const timestamp = this.changeFileame(file.createdAt);
+    //   return !oldFilesTimestamps.includes(timestamp);
+    // });
+    console.log(
+      `-> File Report |Total: ${newFiles.length} | New: ${newFiles.length} | Exist: ${oldVideoFiles.length}`
+    );
+    // TODO Download new files
+    await this.downloadAllFiles(newFiles, user);
+  }
 
-	public async downloadAllFiles(files: UserData[], user: string) {
-		// TODO Check if the user folder exists
-		const directory = path.join(
-			this.rootDirectory,
-			this.outputFolderName,
-			user
-		);
-		await this.checkFolderExists(directory);
-		// Define progress bar
-		const progressBar = new cliProgress.SingleBar({
-			format:
-				"-> Downloading |" +
-				colors.cyan("{bar}") +
-				"| {percentage}% || {value}/{total} Chunks || Speed: {speed}",
-			barCompleteChar: "\u2588",
-			barIncompleteChar: "\u2591",
-			hideCursor: true,
-		});
-		progressBar.start(files.length, 0);
-		for (const file of files) {
-			// TODO Download csv file
-			const csvURL = await this.getURL(file.sensorDataURL);
-			const csvFileName = this.changeFileame(
-				file.sensorDataURL.split("/").pop() as string
-			);
-			await this.downloadFile(csvURL, directory, csvFileName);
-			// TODO Download video file
-			const videoURL = await this.getURL(file.videoInfo.videoURL);
-			const videoFileName = this.changeFileame(
-				file.videoInfo.videoURL.split("/").pop() as string
-			);
-			await this.downloadFile(videoURL, directory, videoFileName);
-			// TODO Update progress bar
-			progressBar.increment();
-		}
-		progressBar.stop();
-		console.log(
-			`-> Download finished | ${files.length} files downloaded | User: ${user}`
-		);
-	}
+  public async getURL(gs: string): Promise<string> {
+    const gsRef = ref(this.storage, gs);
+    const url = await getDownloadURL(gsRef);
+    return url;
+  }
 
-	public async downloadFile(url: string, directory: string, fileName: string) {
-		const downloader = new Downloader({
-			url,
-			directory, //Sub directories will also be automatically created if they do not exist.
-			fileName,
-		});
+  public async downloadAllFiles(files: IRecord[], user: string) {
+    // TODO Check if the user folder exists
+    const directory = path.join(
+      this.rootDirectory,
+      this.outputFolderName,
+      user
+    );
+    await this.checkFolderExists(directory);
+    // Define progress bar
+    const progressBar = new cliProgress.SingleBar({
+      format:
+        "-> Downloading |" +
+        colors.cyan("{bar}") +
+        "| {percentage}% || {value}/{total} Chunks || Speed: {speed}",
+      barCompleteChar: "\u2588",
+      barIncompleteChar: "\u2591",
+      hideCursor: true,
+    });
+    progressBar.start(files.length, 0);
+    for (const file of files) {
+      // TODO Download csv file
+      const csvURL = await this.getURL(file.sensorDataURL);
+      const csvFileName = this.changeFileame(
+        file.sensorDataURL.split("/").pop() as string
+      );
+      await this.downloadFile(csvURL, directory, csvFileName);
+      // TODO Download video file
+      const videoURL = await this.getURL(file.videoURL);
+      const videoFileName = this.changeFileame(
+        file.videoURL.split("/").pop() as string
+      );
+      await this.downloadFile(videoURL, directory, videoFileName);
+      // TODO Update progress bar
+      progressBar.increment();
+    }
+    progressBar.stop();
+    console.log(
+      `-> Download finished | ${files.length} files downloaded | User: ${user}`
+    );
+  }
 
-		try {
-			await downloader.download();
-		} catch (error) {
-			console.log(error);
-		}
-	}
+  public async downloadFile(url: string, directory: string, fileName: string) {
+    const downloader = new Downloader({
+      url,
+      directory, //Sub directories will also be automatically created if they do not exist.
+      fileName,
+    });
+
+    try {
+      await downloader.download();
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
